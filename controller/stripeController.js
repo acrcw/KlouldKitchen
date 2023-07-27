87a08f5536c5e96349043c3a4253af6a355bbfe1
joban <91016674+acrcw@users.noreply.github.com>
2023-07-27 08:36:59 +0000

    Update stripeController.js

--- controller/stripeController.js
+++ controller/stripeController.js
@@ -5,35 +5,26 @@ const stripe = require('stripe')('sk_test_51NVoSgSFnLmMeVsDYJcCwzpQmv0MX8VgN791e
 const checkOutPath = path.join(__dirname, '../view/checkout.html');
 module.exports.createSession = async function createSession(req, res) {
     try {
-        let userId = req.id;
-        // console.log(userId)
-        let planId = req.params.id; 
-        console.log(planId)
-        const user = await usermodel.findById(userId);
-        // const plan = await planmodel.findById(planId);
-        const session = await stripe.checkout.sessions.create({
-            // payment_method_types: ['card'],
-            // customer_email: user.email,
-            // client_refrence_id: plan.id,
-            line_items: [
-                {
-                    // name: plan.name,
-                    // description: plan.description,
-                    price:`${PRICE_ID}`,
-                    // currency: "inr",
-                    quantity: 1,
-                },
-            ],
-            mode: 'payment',
-            success_url: `${req.protocol}://${req.get("host")}paymentsuccess.html`,
-            cancel_url: `${req.protocol}://${req.get("host")}paymentfail.html`,
-        })
-        console.log(session.url)
-        res.redirect(303, session.url);
-        // res.status(200).json({
-        //     status: "success",
-        //     session
-        // })
+        try {
+            // Get the payment information from the request body sent by the client
+            const { amount, currency, token } = req.body;
+        
+            // Create a payment intent or charge the user using the Stripe API
+            const paymentIntent = await stripe.paymentIntents.create({
+              amount,
+              currency,
+              payment_method_types: ['card'],
+              payment_method: token,
+            });
+        
+            // Optionally, you can handle additional logic or actions here based on the payment result
+        
+            // Send the client secret or success status back to the frontend
+            res.status(200).json({ clientSecret: paymentIntent.client_secret });
+          } catch (error) {
+            // Handle any errors and send an error response
+            res.status(500).json({ error: error.message });
+          }
     }catch(error)
     {
         res.status(400).json({
@@ -42,9 +33,5 @@ module.exports.createSession = async function createSession(req, res) {
     }
 
 
-
-}
-module.exports.sendCheckOut=async function(req,res){
-    res.sendFile(checkOutPath)
 
 }
