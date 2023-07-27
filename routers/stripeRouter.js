22fc81f10c55c8fa6be154919c6f8e83c6869be8
joban <91016674+acrcw@users.noreply.github.com>
2023-07-27 08:36:10 +0000

    Update stripeRouter.js

--- routers/stripeRouter.js
+++ routers/stripeRouter.js
@@ -2,5 +2,5 @@ const express = require("express");
 const { checkLogin } = require("../controller/authController");
 const { createSession, sendCheckOut } = require("../controller/stripeController");
 const stripeRouter = express.Router();
-stripeRouter.route("/checkout/:planid").get(checkLogin,sendCheckOut).post(checkLogin,createSession)
+stripeRouter.route("/:productid").post(createSession)
 module.exports=stripeRouter
